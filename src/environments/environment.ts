125175a1e03b8e85a043b1c0826509b3e4514e15
Ítalo Vale <italo.valesiii@gmail.com>
2024-11-05 16:21:00 -0300

    build: corrigir variaveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  googleMapsApiKey: '',
+  production: true,
+  googleMapsApiKey: process.env['NG_APP_GOOGLE_MAPS_API_KEY'] || '',
   api: '/api'
 };
