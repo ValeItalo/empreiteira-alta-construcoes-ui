aa34fd0948aa88c0adb5cc5cc1feb0a9ff44a4ac
Ítalo Vale <italo.valesiii@gmail.com>
2025-05-25 15:15:51 -0300

    test: atualizar arquivo de teste

--- src/app/component/contact-form/contact-form.service.spec.ts
+++ src/app/component/contact-form/contact-form.service.spec.ts
@@ -1,16 +1,20 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ContactFormService } from './contact-form.service';
 
 describe('Service: ContactForm', () => {
+  let service: ContactFormService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ContactFormService]
     });
+
+    service = TestBed.inject(ContactFormService);
   });
 
-  it('should ...', inject([ContactFormService], (service: ContactFormService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
